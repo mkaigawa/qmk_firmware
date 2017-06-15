78cf6a09ecdca2bbdfa7ce449616c5592a3fd459
mkaigawa <mkaigawa@users.noreply.github.com>
2017-06-15 14:26:51 +0900

    typo

--- keyboards/ergodox/keymaps/mkaigawa_win10/keymap.c
+++ keyboards/ergodox/keymaps/mkaigawa_win10/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_ESC,
         KC_PIPE,        KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(SYMB),
         KC_LCTL,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LCTL(KC_SPC),
+        KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   LCTL(KC_SPC),
         KC_LALT,        KC_QUOT,      LALT(KC_LSFT),  KC_LEFT,KC_RGHT,
                                                       KC_ESC, KC_LGUI,
                                                               KC_HOME,
